4d72fb387b1d592b8bf5e59bc6d21d7be4268d1b
Kmiet <krzys.kmiecik@gmail.com>
2021-04-26 09:27:22 +0200

    EKS cluster config

--- deploy-hyperflow.sh
+++ deploy-hyperflow.sh
@@ -2,5 +2,7 @@ kubectl apply -f tests/old-yamls/minikube/crb.yml &&
 kubectl apply -f tests/old-yamls/minikube/cm.yml &&
 kubectl apply -f tests/old-yamls/minikube/redis-service.yml &&
 kubectl apply -f tests/old-yamls/minikube/redis.yml &&
-kubectl apply -f efs-helm/csi-driver/pvc.yml
-# kubectl apply -f tests/old-yamls/minikube/nfs-server.yml
+# kubectl apply -f efs-helm/csi-driver/pvc.yml
+kubectl apply -f tests/old-yamls/minikube/nfs-server-service.yml &&
+kubectl apply -f tests/old-yamls/minikube/nfs-server.yml #&&
+# kubectl apply -f tests/old-yamls/minikube/pv-pvc.yml
