0f84f204901f1ade9ad97d5552cc281ead19fb80
Kmiet <krzys.kmiecik@gmail.com>
2021-06-09 19:41:25 +0200

    Parser job

--- deploy-hyperflow-w-selectors.sh
+++ deploy-hyperflow-w-selectors.sh
@@ -2,7 +2,9 @@ kubectl apply -f tests/old-yamls/cluster/crb.yml &&
 kubectl apply -f tests/old-yamls/cluster/cm.yml &&
 kubectl apply -f tests/old-yamls/cluster/redis-service.yml &&
 kubectl apply -f tests/old-yamls/cluster/redis.yml &&
-# kubectl apply -f efs-helm/csi-driver/pvc.yml
 kubectl apply -f tests/old-yamls/cluster/nfs-server-service.yml &&
 kubectl apply -f tests/old-yamls/cluster/nfs-server.yml #&&
 # kubectl apply -f tests/old-yamls/cluster/pv-pvc.yml
+# kubectl apply -f tests/old-yamls/cluster/hyperflow-engine-deployment.yml
+# kubectl apply -f tests/old-yamls/cluster/parser-job.yml
+# kubectl apply -f tests/old-yamls/cluster/viz-trace-job.yml
